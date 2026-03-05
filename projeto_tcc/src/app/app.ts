0c726c849bc85c88454cbfad4842e9803a11bdab
Malu-git-11 <malumendesrodrigues13@gmail.com>
2026-03-05 13:03:18 -0300

    Reformulando o projeto, tela de login adicionada. Verificar se a barra de pesquisa também foi.

--- projeto_tcc/src/app/app.ts
+++ projeto_tcc/src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, Inject, PLATFORM_ID, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgIf } from '@angular/common'; 
+import { FormsModule } from '@angular/forms';
 import { isPlatformBrowser } from '@angular/common';
 import { Menu } from './menu/menu'
 import { Mapa } from './mapa/mapa';
+import { HttpClientModule } from '@angular/common/http';
+
+imports: [
+  HttpClientModule
+]
 
 @Component({
   selector: 'app-root',
--- projeto_tcc/src/app/mapa/mapa.ts
+++ projeto_tcc/src/app/mapa/mapa.ts
@@ -1,74 +1,49 @@
-import { Component, OnInit, ElementRef, Inject, PLATFORM_ID, signal } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-mapa',
-  templateUrl: './mapa.html',
-  styleUrls: ['./mapa.css'],
+  templateUrl: './mapa.html'
 })
-export class Mapa implements OnInit {
-  public map!: L.Map;
-  public userMarker: L.Marker<any> | null = null;
-  public userCircle: L.Circle<any> | null = null;
+export class MapComponent implements OnInit {
 
-  constructor(
-    public el: ElementRef,
-    @Inject(PLATFORM_ID) public platformId: Object
-  ) { }
+  map!: L.Map;
+  searchAddress: string = '';
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      // Inicializar o mapa quando estiver no navegador
-      this.map = L.map(this.el.nativeElement.querySelector('#map')).setView([51.505, -0.09], 13);
 
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      }).addTo(this.map);
+    this.map = L.map('map').setView([-23.5505, -46.6333], 13);
 
-      // Geolocalização
-      if (navigator.geolocation) {
-        const options = {
-          enableHighAccuracy: true,
-          timeout: 5000,
-          maximumAge: 0,
-        };
-        navigator.geolocation.watchPosition(
-          (position) => this.success(position),
-          (error) => this.error(error),
-          options
-        );
-      } else {
-        console.error('Seu navegador não suporta geolocalização');
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'OpenStreetMap'
+    }).addTo(this.map);
+
+  }
+
+  searchLocation() {
+
+    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${this.searchAddress}`;
+
+    this.http.get<any[]>(url).subscribe(result => {
+
+      if (result.length > 0) {
+
+        const lat = result[0].lat;
+        const lon = result[0].lon;
+
+        this.map.setView([lat, lon], 16);
+
+        L.marker([lat, lon]).addTo(this.map)
+          .bindPopup(this.searchAddress)
+          .openPopup();
+
       }
-    }
+
+    });
+
   }
 
-  public success(position: GeolocationPosition) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const accuracy = position.coords.accuracy;
-    const latlon: L.LatLngTuple = [lat, lon];
-
-    if (!this.userMarker) {
-      this.map.setView(latlon, 16);
-    }
-
-    if (this.userMarker) {
-      this.userMarker.setLatLng(latlon);
-    } else {
-      this.userMarker = L.marker(latlon).addTo(this.map).bindPopup("You're here!").openPopup();
-    }
-
-    if (this.userCircle) {
-      this.userCircle.setLatLng(latlon).setRadius(accuracy);
-    } else {
-      this.userCircle = L.circle(latlon, { radius: accuracy }).addTo(this.map);
-    }
-  }
-
-  public error(err: GeolocationPositionError) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-  }
 }
